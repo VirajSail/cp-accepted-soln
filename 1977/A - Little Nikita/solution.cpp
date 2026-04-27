7c0c0cb54a0026d776ab27b70ef40f4aac10f4cb
VirajSail <virajsail100@gmail.com>
2026-04-28 02:00:00 +0530

    Add Little Nikita [A] from Codeforces

---
+++ 1977/A - Little Nikita/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+        if( n > m){
+        if( n % 2 == 0 && m % 2 == 0){
+            cout << "YES" << endl;
+        }
+        else if( n % 2 != 0 && m % 2 != 0){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+    else if( m > n){
+        cout << "NO" << endl;
+    }
+    else if( n == m){
+        cout << "YES" << endl;
+    }
+    }
+}
