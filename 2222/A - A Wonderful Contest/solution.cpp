8d45c4adc3418e6596b8b2287198b922b79bd2a6
VirajSail <virajsail100@gmail.com>
2026-04-28 02:03:04 +0530

    Add A Wonderful Contest [A] from Codeforces

---
+++ 2222/A - A Wonderful Contest/solution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+ 
+    while(t--){
+        int n;
+        cin >> n;
+ 
+        bool ok = false;
+ 
+        for(int i = 0; i < n; i++){
+            int a;
+            cin >> a;
+ 
+            if(a == 100){
+                ok = true;
+            }
+        }
+ 
+        if(ok) cout << "Yes
+";
+        else cout << "No
+";
+    }
+ 
+    return 0;
+}
