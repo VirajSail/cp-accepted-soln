a09e1bb6c40e4b5de3e097332b4e1d9daa92a76f
VirajSail <virajsail100@gmail.com>
2026-04-28 02:02:31 +0530

    Add Chips [A] from Codeforces

---
+++ 92/A - Chips/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    int i = 1;
+    while (m >= i) {
+        m -= i;
+        i++;
+        if (i > n) i = 1;
+    }
+    
+    cout << m;
+    return 0;
+}
