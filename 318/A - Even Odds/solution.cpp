0b389394c064036a4334f5afa51e9ee20dfeb298
VirajSail <virajsail100@gmail.com>
2026-04-28 02:00:37 +0530

    Add Even Odds [A] from Codeforces

---
+++ 318/A - Even Odds/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+   long long n, k;
+   cin >> n >> k;
+   long long mid = (n + 1) / 2;
+   if(k <= mid){
+       cout << 2 * k - 1 << endl;
+   }
+   else{
+       cout << 2 * (k - mid) << endl;
+   }
+}
