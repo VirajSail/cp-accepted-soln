948db1ed54d6f0c7432356ab7ea57362c3992568
VirajSail <virajsail100@gmail.com>
2026-04-28 02:00:21 +0530

    Add Maple and Multiplication [A] from Codeforces

---
+++ 2139/A - Maple and Multiplication/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        if(a == b){
+            cout << 0 << endl;
+        }
+        else if(a == 1 || b == 1){
+            cout << 1 << endl;
+        }
+        else if( b % a == 0 || a % b == 0){
+            cout << 1 << endl;
+        }
+        else{
+            cout << 2 << endl;
+        }
+    }
+}
