d7c018dbf899160d5737d0e49a927946fdc43cef
VirajSail <virajsail100@gmail.com>
2026-04-30 20:16:04 +0530

    Add Koshary [A] from Codeforces

---
+++ 2227/A - Koshary/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int x, y;
+        cin >> x >> y;
+        if(y % 2 == 0 || x % 2 == 0){
+            cout << "yes" << endl;
+        }
+        else{
+            cout << "no" << endl;
+        }
+    }
+}
