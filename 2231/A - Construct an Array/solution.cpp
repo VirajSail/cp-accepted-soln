298ff0f8ba1de95c291d1ff64d32358fe2b9900f
VirajSail <virajsail100@gmail.com>
2026-05-21 20:16:22 +0530

    Add Construct an Array [A] from Codeforces

---
+++ 2231/A - Construct an Array/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main(){
+    int t; cin >> t;
+    while (t--){
+        int n; cin >> n;
+        for(int i = 1; i <= n; ++i){
+            cout << 2 * i - 1 << " ";
+        }
+        cout << endl;
+ 
+    }
+}
