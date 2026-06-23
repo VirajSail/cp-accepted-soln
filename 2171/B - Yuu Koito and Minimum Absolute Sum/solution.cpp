48d28c3b5b106a89a88bb862ce4b2a57f383b3af
VirajSail <virajsail100@gmail.com>
2026-06-24 00:47:23 +0530

    Add Yuu Koito and Minimum Absolute Sum [B] from Codeforces

---
+++ 2171/B - Yuu Koito and Minimum Absolute Sum/solution.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vector<int> a(n), b;
+        for(int i = 0; i < n; ++i){
+            cin >> a[i];
+        }
+        if(a[n - 1] == -1 && a[0] != -1){
+            a[n - 1] = a[0];
+        }
+        else if(a[n - 1] == -1 && a[0] == -1){
+            a[n - 1] = 0;
+        }
+        if(a[0] == -1 && a[n - 1] != -1){
+            a[0] = a[n -1];
+        }
+        for(int i = 0; i < n; ++i){
+            if(a[i] == -1 && i != n - 1){
+                a[i] = 0;
+            }
+        }
+        ll kk = 0, sum = 0;
+        for(int i = 0; i < n - 1; ++i){
+            b.push_back(a[i + 1] - a[i]);
+ 
+        }
+        for(int i = 0; i < b.size(); ++i){
+            sum += b[i];
+        }
+        cout << abs(sum) << endl;
+        for(auto x : a) cout << x << " ";
+        cout << endl;
+    }
+}
