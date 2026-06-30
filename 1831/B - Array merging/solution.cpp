faaef09540a8bf34687336234655eb8d76522776
VirajSail <virajsail100@gmail.com>
2026-07-01 01:22:35 +0530

    Add Array merging [B] from Codeforces

--- 1831/B - Array merging/solution.cpp
+++ 1831/B - Array merging/solution.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
- 
+ 
 const ll MOD = 1e9 + 7;
 const ll INF = 1e18;
 const int N = 1e6 + 5;
- 
+ 
 ll ans = INF;
 ll sum = 0;
 vector<ll> a;
 set<string> st;
 vector<pair<int, int>> bb;
- 
+ 
 void solve(){
- 
+ 
 }
- 
+ 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
- 
+ 
     int t; cin >> t;
     while(t--){
         int n; cin >> n;
