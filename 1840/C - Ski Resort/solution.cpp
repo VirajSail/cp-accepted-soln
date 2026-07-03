aad5961be95ea0bbe11aae70c400d1785ada6fed
VirajSail <virajsail100@gmail.com>
2026-07-04 02:10:03 +0530

    Add Ski Resort [C] from Codeforces

--- 1840/C - Ski Resort/solution.cpp
+++ 1840/C - Ski Resort/solution.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
- 
+ 
 const ll MOD = 676767677;
 const ll INF = 1e18;
 const int N = 1e6 + 5;
- 
+ 
 ll ans = INF;
 set<string> st;
 vector<pair<int, int>> bb;
- 
+ 
 ll solve(vector<ll> &a, ll last, ll end, ll k){
     if((end - last + 1) < k) return 0;
     else{
@@ -17,13 +17,13 @@ ll solve(vector<ll> &a, ll last, ll end, ll k){
         return (m - k + 1) * (m - k + 2) / 2;
     }
 }
- 
- 
- 
+ 
+ 
+ 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
- 
+ 
     int t; cin >> t;
     while(t--){
         ll n, k, q; cin >> n >> k >> q;
@@ -41,6 +41,6 @@ int main(){
             }
         }
         sum += solve(a, last, n - 1, k);
-        cout << sum << endl;
+        cout << sum  << endl;
     }
 }
