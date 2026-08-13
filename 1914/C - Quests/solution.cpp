68d7892d4ce45385e821e9e9fd7ede7171373618
VirajSail <virajsail100@gmail.com>
2026-08-13 23:32:15 +0530

    Add Quests [C] from Codeforces

---
+++ 1914/C - Quests/solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+ 
+const ll MOD = 676767677;
+const ll INF = 1e18;
+const int N = 1e6 + 5;
+ 
+ll ans = INF;
+set<string> st;
+vector<pair<int, int>> bb;
+ 
+map<int, ll> memo;
+ 
+void solve(){
+    int n, k; cin >> n >> k;
+    vector<ll> a(n), b(n);
+    for(int i = 0; i < n; ++i){
+        cin >> a[i];
+    }
+    for(int i = 0; i < n; ++i){
+        cin >> b[i];
+    }
+    ll mx = 0, count = 0, ans = 0, sum = 0;
+    for(int i = 0; i < min(n, k); ++i){
+        sum += a[i];
+        mx = max(mx, b[i]);
+        count = k -(i + 1);
+        ans = max(ans, sum + count * mx);
+    }
+    cout << ans << '
+';
+}
+ 
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+    
+}
